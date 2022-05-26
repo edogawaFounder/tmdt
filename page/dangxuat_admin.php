9a3c612d62bae243fd5ee6828f3d79a9eda99226
edogawaFounder <you@example.com>
2022-05-26 17:07:56 +0700

    ok

--- page/dangxuat_admin.php
+++
@@ -1,7 +0,0 @@
-<?php session_start();
- unset($_SESSION["id_staff"]);
- unset($_SESSION["name_staff"]);
- unset($_SESSION["address"]);
- unset($_SESSION["phone"]);
-header("Location: index.php");
-?>
